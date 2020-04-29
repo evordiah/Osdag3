5f4fe48946e44421f9af99067e2e8be69e47bcad
nitin <ns3098@bennett.edu.in>
2020-04-29 17:31:13 +0530

    check

--- Module_test.py
+++ Module_test.py
@@ -126,14 +126,14 @@ if __name__ == '__main__':
 
 
 
-    with open(log_file, 'r') as content_file:
-        content = content_file.read()
+    #with open(log_file, 'r') as content_file:
+        #content = content_file.read()
 
     '''
         Reading the log file to see the output on console rather than opening the log file to see the output.
         In actual test environment we won't need it.
     '''
-    print(content)
+    #print(content)
 
     test_exit_code = int(not result.wasSuccessful())
     #print('Exit Status Code is : ',test_exit_code)
