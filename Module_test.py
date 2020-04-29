be1a5e7d88f2892e981ff8221431653edfcd2d55
nitin <ns3098@bennett.edu.in>
2020-04-29 16:47:07 +0530

    push

--- Module_test.py
+++ Module_test.py
@@ -136,6 +136,6 @@ if __name__ == '__main__':
     print(content)
 
 
-    #test_exit_code = int(not result.wasSuccessful())
-    #sys.exit(test_exit_code)                                       # This step is important for travis CI.
+    test_exit_code = int(not result.wasSuccessful())
+    sys.exit(test_exit_code)                                       # This step is important for travis CI.
     #print('Exit Status Code is : ',test_exit_code)
