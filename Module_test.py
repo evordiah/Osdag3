44a1b3d160223c22c50c949cb679aca088505689
nitin <ns3098@bennett.edu.in>
2020-05-03 13:22:12 +0530

    Testing

--- Module_test.py
+++ Module_test.py
@@ -5,32 +5,13 @@ import sys
 import unittest
 
 from design_type.connection.fin_plate_connection import FinPlateConnection
-from design_type.connection.cleat_angle_connection import CleatAngleConnection
-from design_type.connection.seated_angle_connection import SeatedAngleConnection
-from design_type.connection.end_plate_connection import EndPlateConnection
-from design_type.connection.base_plate_connection import BasePlateConnection
-
-from design_type.connection.beam_cover_plate import BeamCoverPlate
-from design_type.connection.beam_cover_plate_weld import BeamCoverPlateWeld
-from design_type.connection.column_cover_plate_weld import ColumnCoverPlateWeld
-
-from design_type.tension_member.tension_bolted import Tension_bolted
-from design_type.tension_member.tension_welded import Tension_welded
-from design_type.connection.beam_end_plate import BeamEndPlate
-from design_type.connection.column_cover_plate import ColumnCoverPlate
-from design_type.connection.column_end_plate import ColumnEndPlate
-from design_type.compression_member.compression import Compression
 
 
 
 
 
 
-all_modules = {'Base Plate':BasePlateConnection, 'Beam Coverplate  Weld Connection':BeamCoverPlateWeld,'Beam Coverplate Connection':BeamCoverPlate,
-    'Cleat Angle':CleatAngleConnection, 'Column Coverplate Weld Connection':ColumnCoverPlateWeld, 'Column Coverplate Connection':ColumnCoverPlate,
-    'Column Endplate Connection':ColumnEndPlate, 'End Plate':EndPlateConnection, 'Fin Plate':FinPlateConnection,'Seated Angle': SeatedAngleConnection,
-    'Tension Members Bolted Design':Tension_bolted, 'Tension Members Welded Design':Tension_welded, 'Compression Member':Compression,
-    }
+
 
 
 '''
