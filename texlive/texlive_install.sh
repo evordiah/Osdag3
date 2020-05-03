b35d3f0f976a275a6fb9c7a4c64cc05c6142af3c
nitin <ns3098@bennett.edu.in>
2020-05-03 17:27:19 +0530

    testing

--- texlive/texlive_install.sh
+++ texlive/texlive_install.sh
@@ -21,18 +21,93 @@ if ! command -v texlua > /dev/null; then
   cd ..
 fi
 
-# Just including texlua so the cache check above works
-# Needed for any use of texlua even if not testing LuaTeX
-tlmgr install luatex
+tlmgr update --self
 
-# Other contrib packages: done as a block to avoid multiple calls to tlmgr
-# texlive-latex-base is needed to run pdflatex
-tlmgr install   \
-  exam          \
+# The test framework itself
+tlmgr install l3build
+
+# Required to build plain and LaTeX formats including (u)pLaTeX
+tlmgr install latex-bin luahbtex platex uplatex tex xetex
+
+# Then get the rest of required LaTeX
+tlmgr install amsmath graphics tools 
+
+# Assuming a 'basic' font set up, metafont is required to avoid
+# warnings with some packages and errors with others
+tlmgr install metafont mfware texlive-scripts
+
+# Contrib packages for testing
+# The deps here will include l3... but that's not worth worrying about
+tlmgr install \
+  chemformula \
+  ctex        \
+  mhchem      \
+  siunitx     \
+  unicode-math
+# Dependencies for tests that are not auto-resolved:
+# a lot for CTeX as different engine need different stuff,
+# also fonts as they are not down as deps
+tlmgr install \
+  adobemapping  \
+  alphalph      \
   amsfonts      \
-  stmaryrd      \
-  amsmath		\
-  collection-fontsrecommended
+  bibtex        \
+  bitset        \
+  catchfile     \
+  cjk           \
+  cjkpunct      \
+  ctablestack   \
+  ec            \
+  epstopdf-pkg  \
+  fandol        \
+  lualatex-math \
+  ltxcmds       \
+  pdfescape     \
+  pdftexcmds    \
+  pgf           \
+  trimspaces    \
+  ucharcat      \
+  xcolor        \
+  xecjk         \
+  luatexja      \
+  xkeyval       \
+  zhmetrics
+  
+# For the doc target
+tlmgr install \
+  atbegshi    \
+  atveryend   \
+  auxhook     \
+  bigintcalc  \
+  bookmark    \
+  booktabs    \
+  colortbl    \
+  csquotes    \
+  dvips       \
+  enumitem    \
+  etexcmds    \
+  fancyvrb    \
+  gettitlestring \
+  hologo      \
+  hycolor     \
+  hyperref    \
+  intcalc     \
+  kvdefinekeys \
+  kvoptions   \
+  kvsetkeys   \
+  letltxmacro \
+  listings    \
+  makeindex   \
+  mathpazo    \
+  metalogo    \
+  oberdiek    \
+  psnfss      \
+  ragged2e    \
+  refcount    \
+  rerunfilecheck \
+  times       \
+  underscore  \
+  uniquecounter
 
 # Keep no backups (not required, simply makes cache bigger)
 tlmgr option -- autobackup 0
