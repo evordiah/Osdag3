73ce0cb336ea10b708793d1d18caea1ed2740b87
nitin <ns3098@bennett.edu.in>
2020-05-05 23:33:07 +0530

    test

--- df.py
+++ df.py
@@ -1,3 +1,45 @@
 import numpy
-from OCC.Core import *
+from OCC.Core.BRepBuilderAPI import BRepBuilderAPI_MakeEdge, BRepBuilderAPI_MakeWire, BRepBuilderAPI_MakeFace
+
+from OCC.Core.gp import gp_Dir, gp_Circ, gp_Ax2
+from OCC.Core.BRepAlgoAPI import BRepAlgoAPI_Fuse
+
+from OCC.Core.gp import gp_Ax1
+from OCC.Core.BRepPrimAPI import *
+
+
+from OCC.Core.BRepPrimAPI import BRepPrimAPI_MakeCylinder
+from OCC.Core.gp import gp_Ax2
+from OCC.Core.BRepAlgoAPI import BRepAlgoAPI_Fuse
+
+from OCC.Core.BRepPrimAPI import BRepPrimAPI_MakeBox
+
+
+from OCC.Core.BRepAlgoAPI import BRepAlgoAPI_Cut
+from OCC.Core.BRepAlgoAPI import BRepAlgoAPI_Cut
+
+from OCC.Core.gp import gp_Ax1
+from OCC.Core.BRepPrimAPI import BRepPrimAPI_MakeRevol
+
+from OCC.Core.BRepPrimAPI import BRepPrimAPI_MakePrism
+from OCC.Core.gp import gp_Pnt, gp_Dir, gp_Vec
+from OCC.Core.BRepBuilderAPI import BRepBuilderAPI_MakeFace
+from OCC.Core.BRepBuilderAPI import (BRepBuilderAPI_MakeEdge,
+                                BRepBuilderAPI_MakeVertex,
+                                BRepBuilderAPI_MakeWire)
+from OCC.Core.BRepFill import BRepFill_Filling
+from OCC.Core.GeomAbs import GeomAbs_C0
+from OCC.Core.GeomAPI import GeomAPI_PointsToBSpline
+from OCC.Core.TColgp import TColgp_Array1OfPnt
+
+
+from OCC.Core.gp import gp_Circ, gp_Ax2
+
+from OCC.Core.BRepPrimAPI import BRepPrimAPI_MakeCylinder
+from OCC.Core.gp import gp_Ax2
+from OCC.Core.BRepAlgoAPI import BRepAlgoAPI_Cut
+
+from OCC.Core.gp import gp_Ax1
+from OCC.Core.BRepPrimAPI import BRepPrimAPI_MakeRevol
+
 print('hello')
